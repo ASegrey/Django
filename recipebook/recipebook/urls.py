786455866a624ce702df6968be137da44e716e77
Sergey Avtonomov <avtonomovsergei@yandex.ru>
2025-03-02 19:27:04 +0300

    add five recipes from site

--- recipebook/recipebook/urls.py
+++ recipebook/recipebook/urls.py
@@ -22,4 +22,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('recipe/', include('recipe.urls')),
     path('admin/', admin.site.urls),
+    path('', include('recipe.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
